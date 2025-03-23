10bb699e98ff8894d17add82988aed017e17d32a
Your Name <you@example.com>
2025-03-23 01:46:49 -0300

    new try

--- exercises.c
+++ exercises.c
@@ -164,14 +164,14 @@ int parentesisBalanceados(char *cadena)
       {
          //char *apertura = (char*) pop(pila_apertura) ;
          //char cierre = cadena[k] ;
-         char *apertura = first(pila_apertura) ;
-         char *cierre = first(pila_cierre) ;
+         char *apertura = pop(pila_apertura) ;
+         char *cierre = pop(pila_cierre) ;
 
          if (((*apertura) == '(' && (*cierre) != ')') || ((*apertura) == '[' && (*cierre) != ']') || ((*apertura) == '{' && (*cierre) != '}'))
             return 0 ;
 
-         next(pila_apertura) ;
-         next(pila_cierre) ;
+         //next(pila_apertura) ;
+         //next(pila_cierre) ;
       }
       return 1 ;
    }
